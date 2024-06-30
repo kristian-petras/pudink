67df430a5a669cced2a8c4f03040843785c4aef7
tino <kpetras@tutanota.com>
2024-06-30 12:57:15 +0200

    asset_mapping now initializes images upon start

--- src/pudink/client/asset_mapping.py
+++ src/pudink/client/asset_mapping.py
@@ -1,16 +1,33 @@
+from typing import Optional
+from pyglet.resource import image
+from pyglet.image import Texture
+
+
 class AssetManager:
     def __init__(self) -> None:
-        self.head_mapping = {
-            "1": "simple_head.png",
-            "2": "monkey_head.png",
-            "3": "cow_head.png",
-            "4": "scary_head.png",
-            "5": "minecraft_head.png",
+        self._head_mapping = {
+            1: image("simple_head.png"),
+            2: image("monkey_head.png"),
+            3: image("cow_head.png"),
+            4: image("scary_head.png"),
+            5: image("minecraft_head.png"),
         }
-        self.body_mapping = {
-            "1": "simple_body.png",
-            "2": "monkey_body.png",
-            "3": "cow_body.png",
-            "4": "scary_body.png",
-            "5": "minecraft_body.png",
+        self._body_mapping = {
+            1: image("human_body.png"),
+            2: image("monkey_body.png"),
+            3: image("woman_body.png"),
+            4: image("long_body.png"),
+            5: image("minecraft_body.png"),
         }
+
+    def get_head(self, head_id: int) -> Optional[Texture]:
+        if head_id not in self._head_mapping:
+            print(f"Head id {head_id} not found")
+            return None
+        return self._head_mapping[head_id]
+
+    def get_body(self, body_id: int) -> Optional[Texture]:
+        if body_id not in self._body_mapping:
+            print(f"Body id {body_id} not found")
+            return None
+        return self._body_mapping[body_id]
