cded184078b5bc7ffd9730af88f2620bc83f0c6d
tino <kpetras@tutanota.com>
2024-06-30 13:17:10 +0200

    fixed positioning on player_display

--- src/pudink/client/renderer/player_display.py
+++ src/pudink/client/renderer/player_display.py
@@ -12,38 +12,35 @@ class PlayerDisplay:
         self.x = x
         self.y = y
         self.batch = batch
-        self.head = Sprite(head, x=x, y=y + 32, batch=batch)
-        self.body = Sprite(body, x=x, y=y, batch=batch)
+        self.head = Sprite(head, x=x, y=y + 64, batch=batch)
+        self.body = Sprite(body, x=x, y=y - 32, batch=batch)
         self.chat_bubbles = []
 
     def create_chat_bubble(self, message: str) -> None:
         self.chat_bubbles.append(
             Label(
                 message,
-                x=self.x,
-                y=self.y + 32 + 20 * len(self.chat_bubbles),
+                x=self.x + 32,
+                y=self.y + 128 + 16 * len(self.chat_bubbles),
                 batch=self.batch,
-                anchor_x="center",
-                anchor_y="center",
                 color=(0, 0, 0, 255),
             )
         )
-        schedule_once(lambda _: self._pop_chat_bubble, 5)
 
     def move(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
         self.head.x = x
-        self.head.y = y + 32
+        self.head.y = y + 64
         self.body.x = x
-        self.body.y = y
+        self.body.y = y - 32
         self._move_chat_bubbles(x, y)
 
-    def _pop_chat_bubble(self) -> None:
+    def pop_chat_bubble(self, _) -> None:
         self.chat_bubbles.pop(0).delete()
         self._move_chat_bubbles(self.x, self.y)
 
     def _move_chat_bubbles(self, x: int, y: int) -> None:
         for index, bubble in enumerate(self.chat_bubbles):
-            bubble.x = x
-            bubble.y = y + 32 + 20 * index
+            bubble.x = x + 32
+            bubble.y = y + 128 + 16 * index
