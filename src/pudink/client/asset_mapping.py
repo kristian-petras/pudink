8bb595d321bb11afea7927bdc4991effbf1c1563
tino <kpetras@tutanota.com>
2024-06-30 12:10:49 +0200

    added basic mapping of files

---
+++ src/pudink/client/asset_mapping.py
@@ -0,0 +1,16 @@
+class AssetManager:
+    def __init__(self) -> None:
+        self.head_mapping = {
+            "1": "simple_head.png",
+            "2": "monkey_head.png",
+            "3": "cow_head.png",
+            "4": "scary_head.png",
+            "5": "minecraft_head.png",
+        }
+        self.body_mapping = {
+            "1": "simple_body.png",
+            "2": "monkey_body.png",
+            "3": "cow_body.png",
+            "4": "scary_body.png",
+            "5": "minecraft_body.png",
+        }
